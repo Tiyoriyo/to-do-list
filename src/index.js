3ce762c73329be04d817e91acdf03b3454258b94
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-14 21:14:01 +0400

    Add create to do function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import ToDo from './modules/toDoObject';
 import './style.css';
 
-const myToDo = ToDo('Buy a book', 'urgent', '01-10-1999', 'personal');
-console.log(myToDo);
+const ToDoList = [];
+
+const getInputs = () => {
+  const name = document.querySelector('#nameInput').value;
+  const date = document.querySelector('#dateInput').value;
+  const type = document.querySelector('#typeInput').value;
+
+  return { name, date, type };
+};
+
+function createToDo() {
+  const inputs = getInputs();
+  const { name } = inputs;
+  const { date } = inputs;
+  const { type } = inputs;
+
+  if (!name) {
+    alert('You need to pixk name');
+    return;
+  }
+
+  const newToDo = ToDo(name, false, date, type);
+  ToDoList.push(newToDo);
+  console.log(ToDoList);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', createToDo);
