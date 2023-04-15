b597c1f030b5de24478bbaa3af4afa51793d520e
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-15 15:56:16 +0400

    Remove getProperties function and clean renderBottomItems function

--- src/modules/toDoObject.js
+++ src/modules/toDoObject.js
@@ -1,34 +1,8 @@
 export default function ToDo(name, priority, dueDate, type) {
-  const getProperties = (...args) => {
-    const Arr = [];
-    console.log(...args);
-    // Loop through all Arguments Except name
-    for (let i = 0; i < args.length; i += 1) {
-      switch (args[i]) {
-        case 'name':
-          Arr.push(name);
-          break;
-        case 'priority':
-          Arr.push(priority);
-          break;
-        case 'dueDate':
-          Arr.push(dueDate);
-          break;
-        case 'type':
-          Arr.push(type);
-          break;
-        default:
-          break;
-      }
-    }
-    return Arr;
-  };
-
   return {
     name,
     priority,
     dueDate,
     type,
-    getProperties,
   };
 }
