0f905236e84504f4d512e02cd3ddb4d5671f5ce6
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-19 13:52:02 +0400

    Move toDoSettings code into renderToDo module

--- src/modules/renderToDo.js
+++ src/modules/renderToDo.js
@@ -1,7 +1,33 @@
 import ToDo, { toDoList } from './toDoObject';
-import renderSettings from './toDoSettings';
 import starNoFill from '../images/starNoFill.png';
 import starFill from '../images/star.png';
+
+const mainContainer = document.querySelector('.mainContainer');
+const body = document.querySelector('body');
+
+function addSettingsScreen() {
+  mainContainer.classList.add('blur');
+  const settingsContainer = document.createElement('div');
+  settingsContainer.classList.add('settingsContainer');
+
+  const settingsDiv = document.createElement('div');
+  settingsDiv.classList.add('settingsDiv');
+
+  settingsContainer.append(settingsDiv);
+  body.appendChild(settingsContainer);
+}
+
+function renderSettings(e, i) {
+  addSettingsScreen();
+
+  const settingsDiv = document.querySelector('.settingsDiv');
+  const nameLabel = document.createElement('label');
+  nameLabel.textContent = 'name';
+  const nameInput = document.createElement('input');
+  nameInput.value = toDoList[i].name;
+
+  settingsDiv.append(nameLabel, nameInput);
+}
 
 const getInputs = () => { // Retrieves inputs for To Do
   const name = document.querySelector('#nameInput').value;
