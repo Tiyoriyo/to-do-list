f30c1fa16c3f4760f2398aff0c507a8c5659ef00
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-21 20:29:41 +0400

    Add type, date, time, notes parameters to task factory function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const taskArray = [];
 
-const Task = (name, priority) => {
+const Task = (name, priority, type, date, time, notes) => {
   const task = {};
   task.name = name;
   task.priority = false;
+  task.type = type;
+  task.date = date;
+  task.time = time;
+  task.notes = notes;
   task.setPriority = () => {
     task.priority = !priority;
     console.log(task);
@@ -12,11 +16,10 @@ const Task = (name, priority) => {
   return task;
 };
 
-const createTask = (name, priority) => {
-  const task = Task(name, priority);
+const createTask = (name) => {
+  const task = Task(name);
   taskArray.push(task);
 };
 
-createTask('Run the Erands', false);
+createTask('Run the Errands');
 console.log(taskArray);
-taskArray[0].setPriority();
