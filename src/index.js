80a545f12ac366b71460c4ee465115b6b3cd5c8c
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-21 20:45:52 +0400

    Fix bug where setPriority was not updating object priority status

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const taskArray = [];
 
 const Task = (name, priority, type, date, time, notes) => {
-  const task = {};
-  task.name = name;
-  task.priority = false;
-  task.type = type;
-  task.date = date;
-  task.time = time;
-  task.notes = notes;
+  const task = {
+    name,
+    priority: false,
+    type,
+    date,
+    time,
+    notes,
+  };
+
   task.setPriority = () => {
-    task.priority = !priority;
-    console.log(task);
+    task.priority = !task.priority;
   };
 
   return task;
@@ -20,6 +21,3 @@ const createTask = (name) => {
   const task = Task(name);
   taskArray.push(task);
 };
-
-createTask('Run the Errands');
-console.log(taskArray);
