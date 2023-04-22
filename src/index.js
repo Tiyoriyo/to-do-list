7d0a8e7d76e9cc4fd9cfa8532ca1e3a0938339dd
Vadim Kostrov <vmkvo15@outlook.com>
2023-04-22 22:51:33 +0400

    Update getTodayTasks function

--- src/index.js
+++ src/index.js
@@ -94,8 +94,16 @@ const getAllTasks = () => {
 };
 
 const getTodayTasks = () => {
-  const today = format(new Date(), 'P');
-  const todayTasks = taskArray.filter((task) => format(task.date, 'P') === today);
+  const today = new Date();
+  const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+  const todayTasks = taskArray.filter((task) => {
+    const taskDateTime = task.getDateTime();
+    if (taskDateTime >= today && taskDateTime < tomorrow) {
+      return true;
+    }
+    return false;
+  });
+
   console.log(todayTasks);
 };
 
@@ -152,21 +160,27 @@ function getHourMinute(inputString) {
 }
 
 // const task1 = createTask('1', 'personal', new Date(2023, 3, 29));
-// const task2 = createTask('2', 'social', new Date(2023, 3, 23), '14:22');
+// const task2 = createTask('2', 'social', new Date(2023, 3, 21), '12:20');
 // const task3 = createTask('3', 'work', new Date(2024, 3, 2));
 // const task4 = createTask('4', 'social', new Date(2023, 3, 22));
 // const task5 = createTask('5', 'work', new Date(2033, 3, 2));
 // const task6 = createTask('6', 'work', new Date(2013, 3, 2));
-// const task7 = createTask('7', 'work', new Date(2023, 3, 22), '21:33');
+// const task7 = createTask('7', 'work', new Date(2023, 3, 22), '22:42');
 // const task8 = createTask('8', 'work', new Date(2023, 3, 22));
 
-setInterval(() => {
+const updateItems = () => {
   for (let i = 0; i < taskArray.length; i += 1) {
     if (taskArray[i].getDateTime() <= new Date()) {
       overdueArray.push(taskArray[i]);
       removeTask(i);
     }
   }
+};
+
+updateItems();
+
+setInterval(() => {
+  updateItems();
 }, 1000);
 
 // const timeInput = document.querySelector('#timeInput');
